b7c664663653e42d1c9d237ff65ed03581955bce
Michelle Nyagaka <michellenyagaka@gmail.com>
2025-09-25 21:08:25 +0300

    my js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+findMatching(['Tina', 'Stella', 'Sonia'], 'tina')
+
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+fuzzyMatch(['Tina', 'Stella', 'Sonia', 'Brad'], 'Stella') //partial match
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
+
+
+
+
